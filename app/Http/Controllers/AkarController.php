5492f6c670886d98dbd9358d26aae4fb1adcc93a
Dek Arya <arya.satyad123@gmail.com>
2023-10-03 07:43:11 +0800

    add validation input 0

--- app/Http/Controllers/AkarController.php
+++ app/Http/Controllers/AkarController.php
@@ -12,46 +12,49 @@ class AkarController extends Controller
     {
         // Validasi input dari pengguna
         $validator = Validator::make($request->all(), [
-            'bilangan' => 'required|numeric|min:1',
+            'bilangan' => 'required|numeric|min:0',
         ]);
-
+        
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 400);
         }
-
+        
         $bilangan = $request->input('bilangan');
         $waktuPemrosesanAwal = microtime(true);
         
         // Inisialisasi $kuadrat_manual
         $hasil_kuadrat = 0;
-
-        // Perhitungan manual akar kuadrat
-        $estimasi = $bilangan / 2;
-        for ($i = 0; $i < 1000; $i++) { // Batasi iterasi ke 1000 untuk menghindari perulangan tak terbatas
-            $estimasi_baru = 0.5 * ($estimasi + $bilangan / $estimasi);
-            if (abs($estimasi_baru - $estimasi) < 1e-6) {
-                $hasil_kuadrat = $estimasi_baru;
-                break;
+        
+        if ($bilangan > 0) {
+            // Perhitungan manual akar kuadrat
+            $estimasi = $bilangan / 2;
+            for ($i = 0; $i < 1000; $i++) {
+                $estimasi_baru = 0.5 * ($estimasi + $bilangan / $estimasi);
+                if (abs($estimasi_baru - $estimasi) < 1e-6) {
+                    $hasil_kuadrat = $estimasi_baru;
+                    break;
+                }
+                $estimasi = $estimasi_baru;
             }
-            $estimasi = $estimasi_baru;
         }
-
+        
         $waktuPemrosesanAkhir = microtime(true);
         $waktuPemrosesan = round(($waktuPemrosesanAkhir - $waktuPemrosesanAwal) * 1000, 6); // Waktu dalam milidetik dengan 6 desimal
         $waktuPemrosesan = number_format($waktuPemrosesan, 6, '.', ''); // Format bilangan desimal
-
+        
         // Simpan ke database
         $akarBilangan = new AkarBilangan();
         $akarBilangan->bilangan = $bilangan;
         $akarBilangan->akar =  $hasil_kuadrat;
         $akarBilangan->waktu_pemrosesan = $waktuPemrosesan;
         $akarBilangan->save();
-
+        
         return response()->json([   
             'bilangan' => $bilangan,
             'akar' => $hasil_kuadrat,
             'waktu_pemrosesan' => $waktuPemrosesan,
         ]);
+        
     }
 
     public function getAllData()
