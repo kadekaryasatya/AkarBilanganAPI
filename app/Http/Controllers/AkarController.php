455d09a982454d715615d771038e68e28b0d0b74
Dek Arya <arya.satyad123@gmail.com>
2023-10-31 01:10:35 +0800

    Add new api

--- app/Http/Controllers/AkarController.php
+++ app/Http/Controllers/AkarController.php
@@ -14,12 +14,14 @@ class AkarController extends Controller
         $validator = Validator::make($request->all(), [
             'bilangan' => 'required|numeric|min:0',
         ]);
-        $id = auth()->user()->id;
+        // $id = auth()->user()->id;
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 400);
         }
 
         $bilangan = $request->input('bilangan');
+        $user_id = $request->input('user_id');
+
         $waktuPemrosesanAwal = microtime(true);
 
         // Inisialisasi $kuadrat_manual
@@ -47,13 +49,14 @@ class AkarController extends Controller
         $akarBilangan->bilangan = $bilangan;
         $akarBilangan->akar =  $hasil_kuadrat;
         $akarBilangan->waktu_pemrosesan = $waktuPemrosesan;
-        $akarBilangan->user_id = $id;
+        $akarBilangan->user_id = $user_id;
         $akarBilangan->save();
 
         return response()->json([
             'bilangan' => $bilangan,
             'akar' => $hasil_kuadrat,
             'waktu_pemrosesan' => $waktuPemrosesan,
+            'user_id' => $user_id
         ]);
 
     }
@@ -64,5 +67,80 @@ class AkarController extends Controller
 
         return response()->json($akarBilangan);
     }
+
+    public function getLowestProcessingTime()
+    {
+        // Menggunakan Eloquent untuk mengambil entri dengan waktu pemrosesan terkecil (ASC)
+        $lowestProcessingTimeEntry = AkarBilangan::orderBy('waktu_pemrosesan', 'asc')->first();
+
+        if ($lowestProcessingTimeEntry) {
+            return response()->json($lowestProcessingTimeEntry);
+        } else {
+            return response()->json(['message' => 'No data found'], 404);
+        }
+    }
+
+    public function getHighestProcessingTime()
+    {
+        // Menggunakan Eloquent untuk mengambil entri dengan waktu pemrosesan terbesar (DESC)
+        $highestProcessingTimeEntry = AkarBilangan::orderBy('waktu_pemrosesan', 'desc')->first();
+
+        if ($highestProcessingTimeEntry) {
+            return response()->json($highestProcessingTimeEntry);
+        } else {
+            return response()->json(['message' => 'No data found'], 404);
+        }
+    }
+
+    public function getDataByUserId(Request $request)
+    {
+        // Validasi input dari pengguna
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $user_id = $request->input('user_id');
+
+        // Menggunakan Eloquent untuk mengambil data akar bilangan berdasarkan user_id
+        $akarBilanganByUser = AkarBilangan::where('user_id', $user_id)->get();
+
+        if ($akarBilanganByUser->count() > 0) {
+            return response()->json($akarBilanganByUser);
+        } else {
+            return response()->json(['message' => 'No data found for the specified user_id'], 404);
+        }
+    }
+
+    public function getAverageProcessingTime()
+    {
+        // Jalankan query SQL untuk menghitung rata-rata waktu pemrosesan
+        $result = AkarBilangan::selectRaw('AVG(waktu_pemrosesan) AS rata_rata_waktu_pemrosesan')->first();
+
+        if ($result) {
+            return response()->json($result);
+        } else {
+            return response()->json(['message' => 'No data found'], 404);
+        }
+    }   
+    
+    public function getUserData()
+    {
+        // Menggunakan Eloquent untuk mengambil data user dan jumlah bilangan yang diinput
+        $userData = AkarBilangan::selectRaw('users.nim as nim, COUNT(akar_bilangans.id) as jumlah_bilangan')
+            ->join('users', 'akar_bilangans.user_id', '=', 'users.id')
+            ->groupBy('users.nim')
+            ->get();
+    
+        if ($userData->count() > 0) {
+            return response()->json($userData);
+        } else {
+            return response()->json(['message' => 'No data found'], 404);
+        }
+    }
+    
 
 }
--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,18 @@ Route::post('/hitung-akar', [App\Http\Controllers\AkarController::class, 'hitung
 
 Route::get('/get-all-data', [App\Http\Controllers\AkarController::class, 'getAllData']);
 
+Route::get('/get-lowest-processing-time', [App\Http\Controllers\AkarController::class, 'getLowestProcessingTime']);
+
+Route::get('/get-highest-processing-time', [App\Http\Controllers\AkarController::class, 'getHighestProcessingTime']);
+
+Route::get('/akar-bilangan/by-user', [App\Http\Controllers\AkarController::class,'getDataByUserId']);
+
+Route::get('/average-processing-time', [App\Http\Controllers\AkarController::class,'getAverageProcessingTime']);
+
+Route::get('/user-data', [App\Http\Controllers\AkarController::class, 'getUserData']);
+
+
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/get-all-users', [AuthController::class, 'getAllData']);
+
+
+Route::get('/get-all-users', [AuthController::class, 'getAllUser']);
